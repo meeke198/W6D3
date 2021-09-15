dd9500524b7a7e17e8489826e4762134ba49d282
Emmay <emmayalam1@gmail.com>
2021-09-15 17:09:43 -0400

    finished routes_and_controllers project

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all 
-    render json: @users 
+    @users = User.all
+    render json: @users
 
   end
 
   def create
-
-    user = User.new(params.require(:user).permit(:name, :email))
-  # replace the `user_attributes_here` with the actual attribute keys
+    user = User.new(user_params)
+    # replace the `user_attributes_here` with the actual attribute keys
     if user.save
       render json: user
     else
@@ -17,6 +16,29 @@ class UsersController < ApplicationController
   end
 
   def show
-    render json: params
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email) # Creates strong params
   end
 end
