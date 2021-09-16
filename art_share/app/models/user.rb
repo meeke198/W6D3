dec5d182ddc3d45a12846880769780d998f46d81
Emmay <emmayalam1@gmail.com>
2021-09-16 12:14:29 -0400

    finished phase 1 and phase 2 of art_share

--- art_share/app/models/user.rb
+++ art_share/app/models/user.rb
@@ -8,19 +8,21 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true 
+    validates :username, presence: true, uniqueness: true
 
     has_many :artworks,
         primary_key: :id,
         foreign_key: :artwork_id,
-        class_name: :Artwork
+        class_name: :Artwork,
+        dependent: :destroy
 
     has_many :viewers,
         primary_key: :id,
         foreign_key: :viewer_id,
-        class_name: :ArtworkShare 
+        class_name: :ArtworkShare,
+        dependent: :destroy
 
-    has_many :shared_artworks, 
+    has_many :shared_artworks,
         through: :shares,
         source: :artwork
 
