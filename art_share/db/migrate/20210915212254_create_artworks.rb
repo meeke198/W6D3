87e9141b8ea136d544dacaa09029a5581d4063e8
Emmay <emmayalam1@gmail.com>
2021-09-16 09:25:00 -0400

    finished creating migration tables

--- art_share/db/migrate/20210915212254_create_artworks.rb
+++ art_share/db/migrate/20210915212254_create_artworks.rb
@@ -4,7 +4,8 @@ class CreateArtworks < ActiveRecord::Migration[5.2]
       t.string :title, null: false
       t.string :image_url, null: false
       t.integer :artist_id, null: false
+      t.timestamps
     end
-    add_index :artworks, :artist_id
+    add_index :artworks, [:artist_id, :title], unique: true
   end
 end
